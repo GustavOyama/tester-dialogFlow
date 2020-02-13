6794911580de553ee559055cf7816b8dfb0db8c6
GustavOyama <32442289+GustavOyama@users.noreply.github.com>
2020-02-13 16:48:26 -0300

    first

---
+++ connector.js
@@ -0,0 +1,45 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+
+async function runSample(projectId = 'tester-dialogflow') {
+    
+  // A unique identifier for the given session
+  const sessionId = uuid.v4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: 'registrar',
+        // The language used by the client (en-US)
+        languageCode: 'pt-br',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  
+  console.log(responses);
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+}
+runSample()
